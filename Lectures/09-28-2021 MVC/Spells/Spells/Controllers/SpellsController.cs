d47a2b46639d2802ac8c0cf57073648cc84d0636
Steven Yackel <spazard1@gmail.com>
2021-09-28 15:12:18 -0500

    adds MVC lecture

---
+++ Lectures/09-28-2021 MVC/Spells/Spells/Controllers/SpellsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Spells.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spells.Controllers
+{
+    public class SpellsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
