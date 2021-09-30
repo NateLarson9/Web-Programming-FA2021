3a87fd3fbeff7e3089b6f492c0b22a0dfe31c3cf
Steven Yackel <spazard1@gmail.com>
2021-09-30 15:36:42 -0500

    mvc 2 lecture

--- Lectures/09-30-2021 MVC 2/Spells/Spells/Controllers/SpellsController.cs
+++ Lectures/09-30-2021 MVC 2/Spells/Spells/Controllers/SpellsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
 using Spells.Models;
+using Spells.Services;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -11,9 +13,72 @@ namespace Spells.Controllers
 {
     public class SpellsController : Controller
     {
+        /// <summary>
+        /// This is NOT the way to get a database.
+        /// Stay tuned for assignment 6, dependency injection
+        /// </summary>
+        private static SpellsDatabase spells = new SpellsDatabase();
+
+        static SpellsController()
+        {
+            spells.Add("Levitate");
+            spells.Add("Transmute");
+            spells.Add("Accio");
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(spells);
+        }
+
+        public IActionResult ViewSpell(string id)
+        {
+            if (int.TryParse(id, out int result))
+            {
+                ViewData["id"] = result;
+                return View(spells.Get(result));
+            }
+
+            return RedirectToAction("index");
+        }
+
+        [HttpPost]
+        public IActionResult Add()
+        {
+            if (Request.HasFormContentType)
+            {
+                if (Request.Form.TryGetValue("spellName", out StringValues spellName))
+                {
+                    spells.Add(spellName.ToString());
+                }
+            }
+
+            return RedirectToAction("index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete()
+        {
+            if (!Request.HasFormContentType)
+            {
+                return RedirectToAction("index");
+            }
+
+            if (!Request.Form.TryGetValue("spellIndex", out StringValues spellIndexString))
+            {
+                return RedirectToAction("index");
+            }
+
+            if (!int.TryParse(spellIndexString, out int spellIndex))
+            {
+                return RedirectToAction("index");
+            }
+
+            // don't forget to add bounds checking on the index
+
+            spells.Delete(spellIndex);
+
+            return RedirectToAction("index");
         }
     }
 }
---
+++ Lectures/09-30-2021 MVC 2/Spells/Spells/Services/SpellsDatabase.cs
@@ -0,0 +1,33 @@
+﻿using Spells.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spells.Services
+{
+    public class SpellsDatabase
+    {
+        private List<SpellModel> spells = new List<SpellModel>();
+
+        public void Add(string spell)
+        {
+            this.spells.Add(new SpellModel() { Spell = spell });
+        }
+
+        public int Count()
+        {
+            return this.spells.Count();
+        }
+
+        public SpellModel Get(int index)
+        {
+            return this.spells[index];
+        }
+
+        public void Delete(int index)
+        {
+            this.spells.RemoveAt(index);
+        }
+    }
+}
